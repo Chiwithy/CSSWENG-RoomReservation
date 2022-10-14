41be2dd5619894fe56ccedb913acd77eb82afce3
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-10-14 10:46:57 +0800

    created user db schema

---
+++ models/UserModel.js
@@ -0,0 +1,35 @@
+var mongoose = require ('mongoose');
+
+
+var UserSchema = new mongoose.Schema({
+    userID: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    employeeType: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model ('User', UserSchema);
