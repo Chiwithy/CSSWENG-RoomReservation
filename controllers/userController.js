81aef90d076ddb37fdb2a90bf205d1a85b2443cb
kyla-dc <83800426+kyla-dc@users.noreply.github.com>
2022-10-17 10:58:54 +0800

    Update userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+// TODO import models
+import bcrypt from "bcrypt";
+
+// exports.createUser = (req, res) => {
+//     // TODO add code to create user account from HTML form
+// }
+
+const userController = {
+    
+    // Adds a username to the datbase given a username and password
+    //  - does not implememt passowrd confirmation 
+    //  - does not check if username already exists
+    postRegister: async function (req, res) {
+        try {
+            const hashedPassword = await bcrypt.hash(req.body.password, 10);
+            var username = req.body.username;
+            var password = hashedPassword;
+            var newUser = {
+                username: username,
+                password: password,
+            }
+            User.create(newUser, err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(">>>   postRegister: Successfully added to DB");
+            })
+            //res.redirect('/login');
+        }
+        catch {
+            //res.redirect('/register');
+            console.log(">>>   postRegister: Unsucessful");  
+        }
+    }
+    
+}
+
+export default userController;
