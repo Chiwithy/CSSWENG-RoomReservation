9d3009f3b0e31b94fe70fab6fe966fbe6284cdc4
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-10-17 19:21:49 +0800

    Update index.js
    - added 'passport' initialization.
    - added mongodb connection message

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const hbs = require('hbs');
-const bodyParser = require('body-parser');
-const routes = require('./routes/routes.js');
+const bodyParser = require ('body-parser');
+const dotenv = require ('dotenv');
+const express = require ('express');
+const hbs = require ('hbs');
 const mongoose = require ('mongoose');
+const passport = require ('passport');
+const routes = require ('./routes/routes.js');
+
+
+dotenv.config ();
 
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 dbURL = process.env.DB_URL;
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
-//app.use('/', routes);     //TEMPORARILY DISABLED WHILE ROUTES HAVE NOT YET BEEN ESTABLISHED
-
-dotenv.config();
-
-port = process.env.PORT;
-hostname = process.env.HOSTNAME;
-dbURL = process.env.DB_URL;
-
-app.use(express.static('public'));
-
-mongoose.connect (dbURL, {useNewURLParser: true, useUnifiedTopology: true});
-
-app.listen(port, hostname, function () {
-    console.log('Server is running at:');
-    console.log('http://' + hostname + ':' + port);
+mongoose.connect (dbURL, {useNewURLParser: true, useUnifiedTopology: true}, (error) => {
+    if (error) throw error;
+    else console.log ("Connected to: " + dbURL);
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '\\' + 'index.html');
+const app = express ();
+app.use (express.json());
+app.use (express.urlencoded ({extended: true}));
+app.use (passport.initialize ());
+
+app.use (express.static('public'));
+
+app.set ('view engine', 'hbs');
+hbs.registerPartials (__dirname + '/views/partials');
+//app.use ('/', routes);     //TEMPORARILY DISABLED WHILE ROUTES HAVE NOT YET BEEN ESTABLISHED
+
+app.listen (port, hostname, function () {
+    console.log ('Server is running at:');
+    console.log ('http://' + hostname + ':' + port);
+});
+
+//TEMPORARY LANDING PAGE
+app.get ('/', function(req, res) {
+    res.sendFile (__dirname + '\\' + 'index.html');
 });
