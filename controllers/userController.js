511872a31d9e0c828205ad0465a8116affb6e0cd
kyla-dc <83800426+kyla-dc@users.noreply.github.com>
2022-10-19 16:11:33 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,12 @@
-// TODO import models
 import bcrypt from "bcrypt";
-
-// exports.createUser = (req, res) => {
-//     // TODO add code to create user account from HTML form
-// }
+import User from "../models/UserModel.js";
 
 const userController = {
-    
-    // Adds a username to the datbase given a username and password
-    //  - does not implememt passowrd confirmation 
-    //  - does not check if username already exists
-    postRegister: async function (req, res) {
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            var username = req.body.username;
-            var password = hashedPassword;
+
+    getRegister: function (req, res) { //test of database 
+       
+        {var username = "Me";
+            var password = "Moi";
             var newUser = {
                 username: username,
                 password: password
@@ -24,16 +16,81 @@ const userController = {
                     console.log(err);
                     return;
                 }
-                console.log(">>>   postRegister: Successfully added to DB");
+            console.log("postRegister: Successfully added to DB");
             })
-            //res.redirect('/login');
+            //res.redirect('/login');}
+                
+        res.render("register");
         }
-        catch {
-            //res.redirect('/register');
-            console.log(">>>   postRegister: Unsuccessful");  
-        }
+
+        //real code: res.render("register"); 
     }
-    
-}
+
+    // //------------------------VINCENT CODE---------------------------
+    // checkUsername: async (req, res) => {
+    //     let username = (req.query.username).toUpperCase (); //so that it's not case senstive
+    //     let foundUser = await User.findone ({username: username});
+    //     let values = {
+    //         symbol: false,  //true if username contains invalid symbols
+    //         taken: false    //true if username is taken
+    //     };
+
+
+    //     //Checks if username is already taken
+    //     if (foundUser != null) {
+    //         values.taken = true;
+    //         res.send (values);
+    //         return;
+    //     }
+
+    //     //Checks if the username contains invalid symbols
+    //     for (i = 0; i < username.length; i++) {
+    //         let curAscii = username.charCodeAt (i);
+    //         //If statement checks if the current character is an invalid symbol (listed below)
+    //         if (!(  (curAscii >= 48 && curAscii <= 57) ||   //Digits
+    //                 (curAscii >= 65 && curAscii <= 90) ||   //Uppercase
+    //                 (curAscii >= 97 && curAscii <= 122) ||  //Lowercase
+    //                 (curAscii == 95) ))                     //Underscore
+    //                 {
+    //                 values.symbol = true;
+    //                 res.send (values);
+    //                 return;
+    //             }
+    //     }
+
+    //     res.send (values);
+    // },
+
+
+    //------------------------KYLA CODE---------------------------
+    // Adds a username to the datbase given a username and password
+    //  - does not implememt passowrd confirmation 
+    //  - does not check if username already exists
+    // postRegister: async function (req, res) {
+    //     try {
+    //         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    //         var username = req.body.username;
+    //         var password = hashedPassword;
+    //         var newUser = {
+    //             username: username,
+    //             password: password
+    //         }
+    //         User.create(newUser, err => {
+    //             if (err) {
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             console.log("postRegister: Successfully added to DB");
+    //         })
+    //         //res.redirect('/login');
+    //     }
+    //     catch {
+    //         //res.redirect('/register');
+    //         console.log("postRegister: Unsuccessful");  
+    //     }
+    // }
+
+}; 
 
 export default userController;
+
