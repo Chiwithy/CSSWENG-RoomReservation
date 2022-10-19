0dc907f0e80458a47400797d7e95e02f14158d8b
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-10-19 10:03:09 +0800

    converted to 'import' methods

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const bodyParser = require ('body-parser');
-const dotenv = require ('dotenv');
-const express = require ('express');
-const hbs = require ('hbs');
-const mongoose = require ('mongoose');
-const passport = require ('passport');
-const routes = require ('./routes/routes.js');
+import "dotenv/config";
+import express from "express";
+import hbs from "hbs";
+import mongoose from "mongoose";
+import routes from "./routes/routes.js";
+
+//To be removed til routes/paths are established
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath (import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
-dotenv.config ();
-
-port = process.env.PORT;
-hostname = process.env.HOSTNAME;
-dbURL = process.env.DB_URL;
-
+const app = express ();
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
+const dbURL = process.env.DB_URL;
 
 mongoose.connect (dbURL, {useNewURLParser: true, useUnifiedTopology: true}, (error) => {
     if (error) throw error;
     else console.log ("Connected to: " + dbURL);
 });
 
-const app = express ();
 app.use (express.json());
 app.use (express.urlencoded ({extended: true}));
-app.use (passport.initialize ());
 
 app.use (express.static('public'));
 
+
 app.set ('view engine', 'hbs');
-hbs.registerPartials (__dirname + '/views/partials');
+hbs.registerPartials ( './views/partials');
 //app.use ('/', routes);     //TEMPORARILY DISABLED WHILE ROUTES HAVE NOT YET BEEN ESTABLISHED
 
 app.listen (port, hostname, function () {
@@ -37,5 +38,5 @@ app.listen (port, hostname, function () {
 
 //TEMPORARY LANDING PAGE
 app.get ('/', function(req, res) {
-    res.sendFile (__dirname + '\\' + 'index.html');
+    res.sendFile (__dirname + '\\index.html')
 });
