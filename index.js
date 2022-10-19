67af5dc972a3ea1e914e932889c4905d47326799
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-10-19 10:03:09 +0800

    converted to 'import' methods

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
-import bodyParser from "body-parser";
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
 import hbs from "hbs";
 import mongoose from "mongoose";
-import passport from "passport";
 import routes from "./routes/routes.js";
 
 
-dotenv.config ();
-
+const app = express ();
 const port = process.env.PORT;
 const hostname = process.env.HOSTNAME;
 const dbURL = process.env.DB_URL;
-
 
 mongoose.connect (dbURL, {useNewURLParser: true, useUnifiedTopology: true}, (error) => {
     if (error) throw error;
     else console.log ("Connected to: " + dbURL);
 });
 
-const app = express ();
 app.use (express.json());
 app.use (express.urlencoded ({extended: true}));
-app.use (passport.initialize ());
 
 app.use (express.static('public'));
 
-app.set ('view engine', 'hbs'); 
-// hbs.registerPartials (__dirname + '/views/partials'); //not defined?? 
-app.use ('/', routes);     //TEMPORARILY DISABLED WHILE ROUTES HAVE NOT YET BEEN ESTABLISHED
+
+app.set ('view engine', 'hbs');
+hbs.registerPartials ( './views/partials');
+app.use ('/', routes);
 
 app.listen (port, hostname, function () {
     console.log ('Server is running at:');
     console.log ('http://' + hostname + ':' + port);
 });
-
-//TEMPORARY LANDING PAGE
-// app.get ('/', function(req, res) {
-//     res.sendFile (__dirname + '\\' + 'index.html');
-// });
