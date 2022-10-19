64c81eaf328b9fa7aeca48988ad71d74e413df00
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-10-19 17:07:46 +0800

    added register.js
    back-end javascript file for register

--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,33 @@
 $(document).ready (() => {
-    let usernameTextField; //Assign the value of the text fields
+    let usernameTextField = $('#username')[0]; //Assign the value of the text fields
+    let passwordField1 = $('#password1')[0];
+    let passwordField2 = $('#password2')[0];
 
-    usernameTextField = checkUsername;
+    usernameTextField.onkeyup = checkUsername;
+    usernameTextField.onkeydown = checkUsername;
 
+    passwordField1.onkeyup = confirmPassword;
+    passwordField1.onkeydown = confirmPassword;
+    passwordField2.onkeyup = confirmPassword;
+    passwordField2.onkeydown = confirmPassword;
+
+    console.log ("register js load successful");
 
     function checkUsername () {
         $.get ('/checkUsername', {newUsername: usernameTextField.value}, (invalid) => {
             if (invalid.symbol) {
-                //Code if username contains invalid characters
+                console.log ("username has invalid symbols");
             }
             else if (invalid.taken) {
-                //Code if username is taken (i.e., user[name] already exists)
+                console.log ("username is already taken ._.");
             }
         })
+    };
+
+    function confirmPassword () {
+        if (passwordField1.value == passwordField2.value)
+            console.log ("Is same");
+        else
+            console.log ("Not the same");
     }
 });
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ import controller from '../controllers/userController.js'
 const router = Router();
 
 router.get(`/`, controller.getRegister);
+router.get ('/checkUsername', controller.checkUsername);
 router.get('/register', controller.getRegister);
 router.post(`/register`, controller.postRegister); 
 router.get ('/successfulReg', controller.getSuccess);
