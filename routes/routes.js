e4614bb493721a7cd973ae8be2f84123426fd098
kyla-dc <83800426+kyla-dc@users.noreply.github.com>
2022-10-23 11:50:15 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,10 @@ const router = Router();
 router.get ('/', userController.isLoggedIn, calendarController.getCalendarPage); 
 
 router.get ('/checkUsername', userController.checkUsername);
-
 router.get ('/calendar', userController.isLoggedIn, calendarController.getCalendarPage);
 
 router.get ('/login', userController.isLoggedOut, userController.getLogin);
 router.post ('/login', userController.postLogin);
-
 router.get('/register', userController.getRegister);
 router.post('/register', userController.postRegister); 
 
