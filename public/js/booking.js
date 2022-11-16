251df307a3f2e8b6b35f4f888638b148c54e335e
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-11-16 21:04:41 +0800

    updated booking.js
    added getMeetings() => gets meeting info for that day
    
    added renderMeetings () => renders partial bookview-viewing.hbs //the schedule viewing table
    
    added functions to properly style the reserved time slots (color and interactability)
    
    temporarily commented out edit/delete icons cus out of current sprint's scope (ion wanna worry about them atm 🥴)

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,7 +1,80 @@
-function bookedSlot(id) {
-	document.getElementById(id).style.backgroundColor = '#3159BC';
-	document.getElementById(id).innerHTML =		'<i class="fa-solid fa-pen-to-square" style="font-size:12px;"></i>'
-	document.getElementById(id).innerHTML +=	'<div class="px-1 inline"></div><div class="px-1 inline"></div><div class="px-1 inline"></div>'
-	document.getElementById(id).innerHTML +=	'<i class="fa-solid fa-x" style="font-size:12px;"></i><br>'
-	document.getElementById(id).innerHTML += 	'<b>Booked</b>';
-}
+//Parallel arrays || {rooms} array is parallel to {meetings} array || rooms[0] = "Integrity" => meetings[0][n] = meetings in Integrity
+const rooms = ["Integrity", "Innovation", "Teamwork"];
+let meetings = [];
+let accountType;
+
+$(document).ready (() => {
+    for (let i = 0; i < rooms.length; i++)
+        meetings.push ([]);
+    
+    $.get('/getAccountType', (accType) => {
+        accountType = accType;
+    });
+
+	getMeetings ();
+
+
+
+    function getMeetings () {
+        const params = new URLSearchParams (window.location.search);
+        let year = parseInt (params.get ("year"));
+        let month = parseInt (params.get ("month"));
+        let date = parseInt (params.get ("date"));
+        $.get ("/getMeetings", {year: year, month: month, date: date}, (meetingInfos) => {
+            let i;
+
+            for (i = 0; i < meetingInfos.length; i++) {
+                meetingInfos[i].startTime = new Date (meetingInfos[i].startTime);
+                meetingInfos[i].endTime = new Date (meetingInfos[i].endTime);
+                meetings[rooms.indexOf (meetingInfos[i].meetingRoom)].push (meetingInfos[i]);
+            }
+
+			renderMeetings ();
+        });
+    }
+
+    function renderMeetings () {
+        const params = new URLSearchParams (window.location.search);
+        let year = parseInt (params.get ("year"));
+        let month = parseInt (params.get ("month"));
+        let date = parseInt (params.get ("date"));
+        
+        $.get ("/renderMeetingRows", {meetings: meetings, year: year, month: month, date: date}, (html) => {
+            $("#schedDetails")[0].innerHTML = html;
+            colorBookedSlots ();
+            clickableSlots ();
+        });
+    }
+
+    function colorBookedSlots () {
+        let slots = $('.takenSlot');
+        let i;
+
+        for (let i = 0; i < slots.length; i++) {
+            slots[i].style.backgroundColor = "#3159BC";
+            /*slots[i].innerHTML = '<i class="fa-solid fa-pen-to-square" style="font-size:12px;"></i>'
+            slots[i].innerHTML += '<div class="px-1 inline"></div><div class="px-1 inline"></div><div class="px-1 inline"></div>'
+            slots[i].innerHTML += '<i class="fa-solid fa-x" style="font-size:12px;"></i><br>'//*/
+            slots[i].innerHTML += '<b>Booked</b>';
+        }
+    }
+
+    function clickableSlots () {
+        for (let i = 0; i < rooms.length; i++) {
+            for (let j = 0; j < meetings[i].length; j++) {
+                if (meetings[i][j].username != "") {
+                    $('#' + rooms[i] + "_" + j).click (clickRoom);
+                    $('#' + rooms[i] + "_" + j).css ('cursor', "pointer");
+                }
+            }
+        }
+    }
+
+    function clickRoom () {
+        const bookingID = $(this).attr ("id");
+        const roomInd = rooms.indexOf (bookingID.split ("_")[0]);
+        const ind = parseInt (bookingID.split ("_")[1]);
+        
+        console.log (meetings[roomInd][ind]);
+    }
+});
