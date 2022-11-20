9191fcf928a4558738efb46e6e8d54051abcb753
kyla-dc <83800426+kyla-dc@users.noreply.github.com>
2022-11-20 16:22:21 +0800

    changes made in booking.js

--- public/js/booking.js
+++ public/js/booking.js
@@ -1,8 +1,24 @@
 //Parallel arrays || {rooms} array is parallel to {meetings} array || rooms[0] = "Integrity" => meetings[0][n] = meetings in Integrity
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const rooms = ["Integrity", "Innovation", "Teamwork"];
-let meetings = [];  //YAY <-- meeting array that conatins all filled meetings 
+let meetings = [];  
 let accountType;
+let openEndTimes = []; 
+let openStartTimes = []; 
+let allStartTimes = [ "08:00 AM", "08:30 AM", "09:00 AM",
+                        "09:30 AM", "10:00 AM", "10:30 AM",
+                        "11:00 AM", "11:30 AM", "12:00 NN",
+                        "12:30 PM", "01:00 PM", "01:30 PM",
+                        "02:00 PM", "02:30 PM", "03:00 PM",
+                        "03:30 PM", "04:00 PM", "04:30 PM",
+                        "05:00 PM", "05:30 PM" ];
+let allEndTimes =   [ "08:30 AM", "09:00 AM",
+                        "09:30 AM", "10:00 AM", "10:30 AM",
+                        "11:00 AM", "11:30 AM", "12:00 NN",
+                        "12:30 PM", "01:00 PM", "01:30 PM",
+                        "02:00 PM", "02:30 PM", "03:00 PM",
+                        "03:30 PM", "04:00 PM", "04:30 PM",
+                        "05:00 PM", "05:30 PM", "06:00 PM" ];
 
 $(document).ready (() => {
     for (let i = 0; i < rooms.length; i++)
@@ -25,35 +41,25 @@ $(document).ready (() => {
 	getMeetings ();
 
 
-
-    /////////////////////////////////////////KYLA SPACE 
-
-    //[X] ADD TO DATABASE (based on book buttons)
-    //[X] DYNAMIC START AND END TIMES (based on room selection)
-    //      [] MAKE SURE THAT start < end and end > start ALWAYS (when showing clickable options)
-    //      [X] only show the times that are NOT booked 
-    //[] add a check to confirm that the booking has been added to the database 
-    //[X] disable book button if room is not yet selected 
-
-    //STEP 1: DISABLE BOOK BUTTON UNTIL ALL REQUIRED FIELDS ARE FILLED (specifically room)
+    //PART  1: DISABLE BOOK BUTTON UNTIL ALL REQUIRED FIELDS ARE FILLED (specifically room)
     document.querySelector('#book').disabled = true; 
 
-    //STEP 2: get year,month,day, start and end times and turn them into DATE objects 
+    //PART  2: get year,month,day, start and end times and turn them into DATE objects 
     var currDate = $("#date").text();
     var splitDate = currDate.split(" "); 
     var year = splitDate[2]; 
     var month = months.indexOf(splitDate[1]); 
     var date = splitDate[0]; 
 
-    //STEP 3: when book is clicked, get info 
+    //PART  3: when book is clicked, get info 
     $("#book").on('click', function(){
-        //STEP 4: get start and end time from the form 
+        //PART  4: get start and end time from the form 
         var startSelect = document.getElementById("startTime"); //startTime
         var start = startSelect.options[startSelect.selectedIndex].text; 
         var endSelect = document.getElementById("endTime"); //endTime 
         var end = endSelect.options[endSelect.selectedIndex].text; 
 
-        //STEP 5: turn start and end time into dates 
+        //PART  5: turn start and end time into dates 
         var startSplit = start.split(":"); 
         var startHour = parseInt(startSplit[0]); 
         var reSplit = startSplit[1].split(" "); 
@@ -63,24 +69,24 @@ $(document).ready (() => {
         var reSplit1 = endSplit[1].split(" "); 
         var endMin = parseInt(reSplit1[0]); 
 
-        //STEP 5.1: offset the times for PM  
-        if(startHour >= 1 && startHour <= 5){
+        //PART  5.1: offset the times for PM  
+        if(startHour >= 1 && startHour <= 6){
             startHour = startHour + 12; 
         }
-        if(endHour >= 1 && endHour <= 5){
+        if(endHour >= 1 && endHour <= 6){
             endHour = endHour + 12; 
         }
         
         var startTimeDate = new Date(year, month, date, startHour, startMin); 
         var endTimedate = new Date(year, month, date, endHour, endMin); 
 
-        //STEP 6: set up to get all the values from the form 
+        //PART  6: set up to get all the values from the form 
         var currRoom = document.getElementById("room"); 
         var currRoomText = currRoom.options[currRoom.selectedIndex].text;   
         var indexOfRoom = rooms.indexOf(currRoomText); 
         var numOfMeetingsInDB = meetings[indexOfRoom].length;
 
-        //STEP 7: get all the values from the form 
+        //PART  7: get all the values from the form 
         var meetingID = numOfMeetingsInDB; 
         var username = $("#username").text();
         var startTime = startTimeDate; 
@@ -91,7 +97,7 @@ $(document).ready (() => {
         var meetingStatus = "S"; 
         var attendeeList = $('#attendees').val(); 
  
-        //STEP 8: ADD MEETINGS TO DB 
+        //PART  8: ADD MEETINGS TO DB 
         fetch("/addBookedMeeting?" + new URLSearchParams({
             meetingID: meetingID,
             username: username,
@@ -108,33 +114,19 @@ $(document).ready (() => {
 
     }); 
 
-
-    //STEP 9: have start and end change depending on click of 
+    //PART  9: have start and end change depending on click of 
     $("#room").on('change', function(){
         document.querySelector('#book').disabled = false;
-        $(".select").remove(); 
+        $("#startTime").empty(); 
+        $("#endTime").empty(); 
+        $("#startTime").append("<option value='' disabled selected class='select'>Select</option>");  
+        $("#endTime").append("<option value='' disabled selected class='select'>Select</option>"); 
         var currRoom = $("#room").val(); //value of room
         var currRoomCap = currRoom.charAt(0).toUpperCase() + currRoom.slice(1); 
         var indexOfRoom = rooms.indexOf(currRoomCap); 
        
-        //STEP 10: get a list of all possible start and times 
-        let allStartTimes = [ "08:00 AM", "08:30 AM", "09:00 AM",
-                              "09:30 AM", "10:00 AM", "10:30 AM",
-                              "11:00 AM", "11:30 AM", "12:00 NN",
-                              "12:30 PM", "01:00 PM", "01:30 PM",
-                              "02:00 PM", "02:30 PM", "03:00 PM",
-                              "03:30 PM", "04:00 PM", "04:30 PM",
-                              "05:00 PM", "05:30 PM" ];
-        let allEndTimes =   [ "08:30 AM", "09:00 AM",
-                              "09:30 AM", "10:00 AM", "10:30 AM",
-                              "11:00 AM", "11:30 AM", "12:00 NN",
-                              "12:30 PM", "01:00 PM", "01:30 PM",
-                              "02:00 PM", "02:30 PM", "03:00 PM",
-                              "03:30 PM", "04:00 PM", "04:30 PM",
-                              "05:00 PM", "05:30 PM", "06:00 PM" ];
-        
-
-        //STEP 11: GET A LIST OF ALL START/ENDTIMES IN DB (including the ones that hide under big meetings)
+        //PART  10: get a list of all possible start and times (global variables -- allStartTimes, allEndTimes)
+        //PART  11: GET A LIST OF ALL START/ENDTIMES IN DB (including the ones that hide under big meetings)
         var currRoomArray = meetings[indexOfRoom]; 
         var i,x;
 
@@ -173,10 +165,10 @@ $(document).ready (() => {
                 var endTempHourVal, startInDBHourVal; 
 
                 //offsets time for 1pm - 5pm 
-                if(startTempHour >= 1 && startTempHour <= 5){ 
+                if(startTempHour >= 1 && startTempHour <= 6){ 
                     startTempHourVal = startTempHour + 12; 
                 }
-                if(endInDBHour >= 1 && endInDBHour <= 5){ 
+                if(endInDBHour >= 1 && endInDBHour <= 6){ 
                     endInDBHourVal = endInDBHour + 12; 
                 }
                 else{
@@ -200,10 +192,10 @@ $(document).ready (() => {
 
 
                 //offsets time for 1pm - 5pm 
-                if(endTempHour >= 1 && endTempHour <= 5){ 
+                if(endTempHour >= 1 && endTempHour <= 6){ 
                     endTempHourVal = endTempHour + 12; 
                 }
-                if(startInDBHour >= 1 && startInDBHour <= 5){ 
+                if(startInDBHour >= 1 && startInDBHour <= 6){ 
                     startInDBHourVal = startInDBHour + 12; 
                 }
                 else{
@@ -226,14 +218,15 @@ $(document).ready (() => {
                 }
             }
         }
+
        
-        //STEP 12: compare start/endTimes that already exist in DB and all start/endTimes possible and makes array the conatins what exists (based on index)
+        //PART 12: compare start/endTimes that already exist in DB and all start/endTimes possible and makes array the conatins what exists (based on index)
         for(i=0; i<startInDBArr.length; i++){   //compares start times in DB vs all start times (and makes array with indexes to remove ie. taken up classes)
             for(x=0; x<allStartTimes.length; x++){
                 var inDBHour = startInDBArr[i].getHours(); 
                 var inDBMin = startInDBArr[i].getMinutes(); 
 
-                if(inDBHour > 12 && inDBHour <= 17){ //offsets for 1pm-5pm 
+                if(inDBHour > 12 && inDBHour <= 18){ //offsets for 1pm-5pm 
                     inDBHour = inDBHour - 12; 
                 }
 
@@ -252,7 +245,7 @@ $(document).ready (() => {
                 var inDBHour = endInDBArr[i].getHours(); 
                 var inDBMin = endInDBArr[i].getMinutes(); 
 
-                if(inDBHour > 12 && inDBHour <= 17){ //offsets for 1pm-5pm
+                if(inDBHour > 12 && inDBHour <= 18){ //offsets for 1pm-5pm
                     inDBHour = inDBHour - 12; 
                 }
 
@@ -267,7 +260,7 @@ $(document).ready (() => {
             }
         } 
 
-        //STEP 12: makes new options based on meetings that already exist -- dynamic time (based on allStartTimes array and toRemoveStart)
+        //PART  12: makes new options based on meetings that already exist -- dynamic time (based on allStartTimes array and toRemoveStart)
         for(i=0; i<allStartTimes.length; i++){  //all startTimes minus the ones found in meetings array 
             if(!(toRemoveStart.includes(i))){
                 var test = document.createElement("option"); 
@@ -276,15 +269,19 @@ $(document).ready (() => {
                 var allStartHour = parseInt(split[0]);
                 var split1 = split[1].split(" ");  
                 var allStartMin = parseInt(split1[0]);
-                var allStartVal; 
+                var allStartID; 
+                if(allStartHour >= 1 && allStartHour <= 6){
+                    allStartHour = allStartHour + 12; 
+                }
                 if(allStartMin == 30){
-                    allStartVal = allStartHour + 0.5; 
+                    allStartID = allStartHour + 0.5; 
                 }
                 else{ 
-                    allStartVal = allStartHour; 
+                    allStartID = allStartHour; 
                 }
-                test.value = allStartVal; 
+                test.id = allStartID; 
                 document.getElementById("startTime").appendChild(test);
+                openStartTimes.push(allStartTimes[i]); 
             }
         }
         for(i=0; i<allEndTimes.length; i++){  //all endTimes minus the ones found in meetings array 
@@ -295,28 +292,33 @@ $(document).ready (() => {
                 var allEndHour = parseInt(split[0]);
                 var split1 = split[1].split(" ");  
                 var allEndMin = parseInt(split1[0]);
-                var allEndVal; 
+                var allEndVID; 
+                if(allEndHour >= 1 && allEndHour <= 6){
+                    allEndHour = allEndHour + 12; 
+                }
                 if(allEndMin == 30){
-                    allEndVal = allEndHour + 0.5; 
+                    allEndVID = allEndHour + 0.5; 
                 }
                 else{ 
-                    allEndVal = allEndHour; 
+                    allEndVID = allEndHour; 
                 }
-                test.value = allEndVal; 
+                test.id = allEndVID; 
                 document.getElementById("endTime").appendChild(test);
+                openEndTimes.push(allEndTimes[i]); 
             }
         }
 
-         
     });
 
-    //STEP 12: onclick of start time -- allow only end times after it that are consecutive (no gaps) in dropdown options for endtime 
+    //PART  12: onclick of start time -- allow only end times after it that are consecutive (no gaps) in dropdown options for endtime 
     $("#startTime").on('change', function(){
-        $("#startTime").val(); 
+        finalChangeTimeOptions("endTime", $("#startTime"), openEndTimes); 
     })
 
-    //STEP 13: onclick of end time -- allow only start times after it that are consecutive (no gaps) in dropdown options for startTime 
-    
+    //PART  13: onclick of end time -- allow only start times after it that are consecutive (no gaps) in dropdown options for startTime 
+    $("#endTime").on('change', function(){
+        finalChangeTimeOptions("startTime", $("#endTime"), openStartTimes); 
+    })
 
     //3: make sure that end tims are always after start time
     //   and that start times are always before end time 
@@ -324,9 +326,71 @@ $(document).ready (() => {
     //     console.log($("#startTime").val()); 
     // }) 
     
+    //-----------
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////STOP 
+    function finalChangeTimeOptions(id, origin, openSlots){
+        var i,x;
+        var selected = $(origin).children(":selected").attr("id"); 
+        var toKeep = []; 
 
+        //given one, change the other 
+        var previous = selected; 
+        for(i=0;i<openSlots.length;i++){
+            var arr = openSlots[i].split(":"); 
+            var hour = parseInt(arr[0]); 
+            var arr2 = arr[1].split(" "); 
+            var min = parseInt(arr2[0]);
+            var currVal; 
+
+            if(hour >= 1 && hour <= 6)
+                hour = hour + 12;
+            
+            if(min == 30)
+                currVal = hour + 0.5
+            else
+                currVal = hour; 
+
+            if(currVal > previous){ 
+                if(currVal - previous == 0.5){
+                    toKeep.push(currVal); 
+                    previous = parseFloat(previous) + 0.5; 
+                }
+            }
+        }
+
+        if(id.localeCompare("endTime") == 0)
+            $("#" + id).empty();
+        
+        for(x=0; x<toKeep.length; x++){  //all endTimes minus the ones found in meetings array
+            var arr = toKeep[x].toString().split("."); 
+            var hour = arr[0]; 
+            var min; 
+
+            if(arr[1] == 5){
+                min = "30"; 
+            }
+            else{
+                min = "00"; 
+            }
+
+            if(hour > 12){
+                hour = hour - 12; 
+                hour = "0" + hour; 
+                min = min + " PM"
+            }
+            else if (hour == 12){
+                min = min + " NN"
+            }
+            else{
+                min = min + " AM"
+            }
+            var thisHour = hour + ":" + min; 
+            
+            var newOption = $("<option>").text(thisHour); 
+            newOption.attr('id',toKeep[x]); 
+            $("#" + id).append(newOption); 
+        }
+    }
 
     //gets all possible meetings for that day -- the ones booked already 
     function getMeetings () {
