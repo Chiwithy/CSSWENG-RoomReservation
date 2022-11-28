8da720373f70cbe5a0e156e39f8b274551610d97
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-11-28 22:52:41 +0800

    Update modal.js

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,6 @@
 $(document).ready (() => {
 	var hide = document.getElementsByClassName("close")[0];	// Gets the button for closing the modal box
+	var modal = document.getElementById ("modal");
 
 	// Closes Modal when x is pressed
 	hide.onclick = function() {
