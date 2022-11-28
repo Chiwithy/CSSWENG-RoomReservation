fd2dc75aff197e32d485f6554913b9c714b7fa22
Vincent Avila <77482787+Chiwithy@users.noreply.github.com>
2022-11-28 22:52:27 +0800

    connected confirmation box

--- public/js/confirm.js
+++ public/js/confirm.js
@@ -1,20 +1,9 @@
-var show = document.getElementById("btn");					// Gets the button for opens the modal confirmation box
-var confirmation = document.getElementById("confirmation");	// Gets the modal confirmation box
-var remove = document.getElementsByClassName("delete-button")[0];	// Gets the button for closing the modal confirmation box (delete)
-var cancel = document.getElementsByClassName("cancel-button")[0];	// Gets the button for closing the modal confirmation box (cancel)
+$(document).ready (() => {
+	var confirmation = document.getElementById("confirmation");	// Gets the modal confirmation box
+	var cancel = document.getElementById("confirmClose");	// Gets the button for closing the modal confirmation box (cancel)
 
-// Opens Modal when button is pressed
-show.onclick = function() {
-	confirmation.style.display = "block";
-}
-
-// Closes Modal when confirm is pressed
-remove.onclick = function() {
-	confirmation.style.display = "none";
-	// Add code to delete meeting from schedule and database
-}
-
-// Closes Modal when cancel is pressed
-cancel.onclick = function() {
-	confirmation.style.display = "none";
-}
+	// Closes Modal when cancel is pressed
+	cancel.onclick = function() {
+		confirmation.style.display = "none";
+	}
+});
